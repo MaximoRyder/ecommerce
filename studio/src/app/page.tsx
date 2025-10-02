3b3515ae3385d10424f281cfee5c22158cf1aef2
MaximoRyder <maxigesualdo@gmail.com>
2025-10-02 04:10:44 -0300

    f

--- studio/src/app/page.tsx
+++
@@ -1,5 +0,0 @@
-// This is a placeholder file and should not be rendered directly.
-// The middleware will redirect to the appropriate locale.
-export default function RootPage() {
-  return null;
-}
--- studio/src/firebase/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  "projectId": "studio-9604727534-346ef",
-  "appId": "1:184701460811:web:38b2cdaacb55092c9048ce",
-  "apiKey": "AIzaSyCdug6wWZnja_PdQqeyE7Q1xhsdR71SG7c",
-  "authDomain": "studio-9604727534-346ef.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "184701460811"
-};
